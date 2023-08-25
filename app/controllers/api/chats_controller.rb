8f18aea314004dd528a5fa22a13bd8d8f61829a4
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2023-08-25 12:44:43 +0100

    Implement backend cable

--- app/controllers/api/chats_controller.rb
+++ app/controllers/api/chats_controller.rb
@@ -1,7 +1,23 @@
 module Api
   class ChatsController < ApplicationController
 
-    
+    def index
+      chats = Chat.all
+      render json: chats
+    end
 
+    def create
+      chat = Chat.create(chat_params)
+
+      if chat.save
+        ActionCable.server.broadcast('message_channel', chat )
+      end
+    end
+
+    private
+
+    def chat_params
+      params.require(:chat).permit(:text, :username)
+    end
   end
 end
